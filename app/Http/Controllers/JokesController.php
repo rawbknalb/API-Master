fde3888a37010b849568c5d2b9d7c10f9b0c55da
rawbknalb <Rob@Robins-Mac-Pro.local>
2016-03-20 13:02:51 +0100

    Transforming Data

--- app/Http/Controllers/JokesController.php
+++ app/Http/Controllers/JokesController.php
@@ -14,7 +14,32 @@ class JokesController extends Controller
  public function index(){
    $jokes = Joke::all(); //Not a good idea
    return response()->json([
-     'message' =>  $jokes
    ], 200);
  }
+
+public function show($id){
+  $joke = Joke::find($id);
+
+  if(!$joke){
+    return response()->json([
+      'error' => [
+        'message' => 'Joke does not exist'
+      ]
+    ], 404);
+  }
+  return response()->json([
+  ], 200);
+}
+
+public function transformCollection($jokes){
+  return array_map([$this, 'transform'], $jokes->toArray());
+}
+
+public function transform($joke){
+  return [
+    'joke_id' => $joke['id'],
+    'joke' => $joke['body']
+  ];
+}
+
 }
