5fc835336c49e284c41b1fd1c990ff0f4e20268d
rawbknalb <Rob@Robins-Mac-Pro.local>
2016-03-20 12:18:57 +0100

    Response Message for Jokes

--- app/Http/Controllers/JokesController.php
+++ app/Http/Controllers/JokesController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 use App\Http\Requests;
 use App\Joke;
@@ -12,6 +13,8 @@ class JokesController extends Controller
 
  public function index(){
    $jokes = Joke::all(); //Not a good idea
-   return $jokes;
-}
+   return response()->json([
+     'message' =>  $jokes
+   ], 200);
+ }
 }
